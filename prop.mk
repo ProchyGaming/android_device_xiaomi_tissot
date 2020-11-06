173f4d5a0e4bfdecc27c4f1c64cd9d85131cf7d6
Lucas Dupin <dupin@google.com>
2020-11-06 18:49:49 +0100

    tissot: Enable blur in  dev-options

--- prop.mk
+++ prop.mk
@@ -19,3 +19,9 @@ persist.camera.eis.enable=1
 PRODUCT_PROPERTY_OVERRIDES += \
 persist.vendor.delta_time.enable=true \
 persist.delta_time.enable=true
+
+# Enable blurs, hidden under dev option
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.surface_flinger.supports_background_blur=1 \
+    persist.sys.sf.disable_blurs=1 \
+    ro.sf.blurs_are_expensive=1
